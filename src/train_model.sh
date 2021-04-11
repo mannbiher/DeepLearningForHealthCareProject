437cb06ad987bce13430c8d97b6d4d8996880493
Maneesh Singh <mann.biher@yahoo.co.in>
2021-04-10 21:18:44 -0400

    train model

--- src/train_model.sh
+++ src/train_model.sh
@@ -2,8 +2,8 @@
 python data_preprocess/get_covid_data_dict.py
 python data_preprocess/get_kaggle_data_dict.py
 python data_preprocess/extract_exp_data_crossentropy.py
-python FLANNEL/ensemble_step1.py --arch inception_v3
-python FLANNEL/ensemble_step1.py --arch vgg19_bn
-python FLANNEL/ensemble_step1.py --arch resnext101_32x8d
-python FLANNEL/ensemble_step1.py --arch resnet152
-python FLANNEL/ensemble_step1.py --arch densenet161
+python FLANNEL/ensemble_step1.py --arch=inception_v3 --epochs=10 --crop_size=299 -ck_n=10
+python FLANNEL/ensemble_step1.py --arch resnext101_32x8d --epochs=10 -ck_n=10
+python FLANNEL/ensemble_step1.py --arch resnet152 --epochs=10 -ck_n=10
+python FLANNEL/ensemble_step1.py --arch densenet161 --epochs=10 -ck_n=10
+python FLANNEL/ensemble_step1.py --arch vgg19_bn --epochs=10 -ck_n=10
