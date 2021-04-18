64617a606ad084dcac7cb666e2e9cd9c9b0b19ca
Maneesh Singh <mann.biher@yahoo.co.in>
2021-04-17 23:41:27 -0400

    update workers

--- src/train_model.sh
+++ src/train_model.sh
@@ -4,17 +4,18 @@
 set -x
 epochs=200
 ck_n=50
+workers=6
 for i in $(seq 1 5); do
-    python FLANNEL/ensemble_step1.py --arch inception_v3 --epochs=$epochs --crop_size=299 -ck_n=$ck_n --cv=cv$i
-    python FLANNEL/ensemble_step1.py --arch inception_v3 --epochs=$epochs --crop_size=299 -ck_n=$ck_n --cv=cv$i --test
-    python FLANNEL/ensemble_step1.py --arch resnext101_32x8d --epochs=$epochs -ck_n=$ck_n --cv=cv$i
-    python FLANNEL/ensemble_step1.py --arch resnext101_32x8d --epochs=$epochs -ck_n=$ck_n --cv=cv$i --test
-#    python FLANNEL/ensemble_step1.py --arch resnet152 --epochs=50 -ck_n=10 --cv=cv$i
-#    python FLANNEL/ensemble_step1.py --arch resnet152 --epochs=1 -ck_n=10 --cv=cv$i --test
-#    python FLANNEL/ensemble_step1.py --arch densenet161 --epochs=1 -ck_n=10 --cv=cv$i
-#    python FLANNEL/ensemble_step1.py --arch densenet161 --epochs=1 -ck_n=10 --cv=cv$i --test
-#    python FLANNEL/ensemble_step1.py --arch vgg19_bn --epochs=1 -ck_n=10 --cv=cv$i
-#    python FLANNEL/ensemble_step1.py --arch vgg19_bn --epochs=1 -ck_n=10 --cv=cv$i --test
-#    python FLANNEL/ensemble_step2_ensemble_learning.py --epochs=1 -ck_n=50 --data_dir='./explore_version_03/results/%s_20200407_multiclass_%s' --cv=cv$i
-#    python FLANNEL/ensemble_step2_ensemble_learning.py --epochs=1 -ck_n=50 --data_dir='./explore_version_03/results/%s_20200407_multiclass_%s' --cv=cv$i --test
+    python FLANNEL/ensemble_step1.py --arch inception_v3 --epochs=$epochs --crop_size=299 -ck_n=$ck_n --cv=cv$i -j=$workers
+    python FLANNEL/ensemble_step1.py --arch inception_v3 --epochs=$epochs --crop_size=299 -ck_n=$ck_n --cv=cv$i -j=$workers --test
+    python FLANNEL/ensemble_step1.py --arch resnext101_32x8d --epochs=$epochs -ck_n=$ck_n --cv=cv$i -j=$workers
+    python FLANNEL/ensemble_step1.py --arch resnext101_32x8d --epochs=$epochs -ck_n=$ck_n --cv=cv$i -j=$workers --test
+#    python FLANNEL/ensemble_step1.py --arch resnet152 --epochs=50 -ck_n=10 --cv=cv$i -j=$workers
+#    python FLANNEL/ensemble_step1.py --arch resnet152 --epochs=1 -ck_n=10 --cv=cv$i -j=$workers --test
+#    python FLANNEL/ensemble_step1.py --arch densenet161 --epochs=1 -ck_n=10 --cv=cv$i -j=$workers
+#    python FLANNEL/ensemble_step1.py --arch densenet161 --epochs=1 -ck_n=10 --cv=cv$i -j=$workers --test
+#    python FLANNEL/ensemble_step1.py --arch vgg19_bn --epochs=1 -ck_n=10 --cv=cv$i -j=$workers
+#    python FLANNEL/ensemble_step1.py --arch vgg19_bn --epochs=1 -ck_n=10 --cv=cv$i -j=$workers --test
+#    python FLANNEL/ensemble_step2_ensemble_learning.py --epochs=1 -ck_n=50 --data_dir='./explore_version_03/results/%s_20200407_multiclass_%s' --cv=cv$i -j=$workers
+#    python FLANNEL/ensemble_step2_ensemble_learning.py --epochs=1 -ck_n=50 --data_dir='./explore_version_03/results/%s_20200407_multiclass_%s' --cv=cv$i -j=$workers --test
 done
