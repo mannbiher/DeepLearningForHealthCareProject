1a6a5079bbd5a5266bef6ba71d129da3b0124705
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-28 01:40:32 -0400

    Update inference.py

--- src/data_preprocess/segmentation/inference.py
+++ src/data_preprocess/segmentation/inference.py
@@ -85,8 +85,8 @@ def main():
                     np.save(img_npy_path, image_original)
                     np.save(img_msk_path, post_output[1] + post_output[2])
                     save_dir = header.dir_save
-                    np.save(save_dir + '.image.npy', image_original)
-                    np.save(save_dir + '.mask.npy', post_output[1]+post_output[2])
+                    np.save(save_dir + img_data['path'] + '.image.npy', image_original)
+                    np.save(save_dir + img_data['path'] + '.mask.npy', post_output[1]+post_output[2])
                     formal_dict[img_id]['image_dict'][img_name]['numpy_image_path'] = img_npy_path
                     formal_dict[img_id]['image_dict'][img_name]['numpy_mask_path'] = img_msk_path
 
