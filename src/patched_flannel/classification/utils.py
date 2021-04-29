10c4242827a351b2109d636d672b03c953bc5c83
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-29 13:42:08 -0400

    Update utils.py

--- src/patched_flannel/classification/utils.py
+++ src/patched_flannel/classification/utils.py
@@ -115,7 +115,7 @@ def make_weights_for_balanced_classes_customloader(images_dic, nclasses):
     count = [0] * nclasses
     print(count)
     for image in images_dic:
-        print(image_dic[image])
+        print(images_dic[image])
         count[images_dic[image]] += 1
     weight_per_class = [0.] * nclasses
     N = float(sum(count))
