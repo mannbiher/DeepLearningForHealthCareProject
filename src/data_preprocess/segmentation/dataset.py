17d20bb19c08e955cbbc7e262262ae690a98f534
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-28 23:52:01 -0400

    Update dataset.py

--- src/data_preprocess/segmentation/dataset.py
+++ src/data_preprocess/segmentation/dataset.py
@@ -38,7 +38,7 @@ class SegmentationDataset(Dataset):
         class_dict_keys = list(class_dict.keys())
         class_dict_values = list(class_dict.values())
         class_index = list(filter(lambda i: i > 0, class_dict_values))[0]
-        if len(class_index) > 0 :
+        if len(str(class_index)) > 0 :
             class_name = class_dict_keys[class_index]
         else:
             class_name = 'Uknown'
