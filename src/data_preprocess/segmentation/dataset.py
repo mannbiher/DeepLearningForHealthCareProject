46e00096ba0a4a6b19039f871c5aaa34c38f3b46
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-28 23:50:26 -0400

    Update dataset.py

--- src/data_preprocess/segmentation/dataset.py
+++ src/data_preprocess/segmentation/dataset.py
@@ -37,8 +37,11 @@ class SegmentationDataset(Dataset):
         class_dict = self.formal_dict[og_id]['class']
         class_dict_keys = list(class_dict.keys())
         class_dict_values = list(class_dict.values())
-        class_index = [x for x in range(len(class_dict_values)) if class_dict_values[x] > 0 else 'Unknown']
-        class_name = class_dict_keys[class_index[0]]
+        class_index = list(filter(lambda i: i > 0, class_dict_values))[0]
+        if len(class_index) > 0 :
+            class_name = class_dict_keys[class_index]
+        else:
+            class_name = 'Uknown'
         print(class_dict)
         print(class_name)
         images = np.asarray(Image.fromarray(images).resize((header.resize_width, header.resize_height)))
