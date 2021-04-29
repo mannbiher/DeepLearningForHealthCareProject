3ac375d03ffc4021b6d0b176fdbed1bd65433026
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-29 13:47:07 -0400

    Update header.py

--- src/patched_flannel/classification/header.py
+++ src/patched_flannel/classification/header.py
@@ -3,8 +3,11 @@ test_name = 'classifier_newset'
 sampling_option = 'oversampling'
 
 # Batch size
-train_batch_size = 16
-val_batch_size = 16
+#train_batch_size = 16
+#val_batch_size = 16
+#test_batch_size = 1
+train_batch_size = 1
+val_batch_size = 1
 test_batch_size = 1
 
 # Num_classes
