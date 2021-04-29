9658ac29a45aa887ea46ce15c6c4e82388bf4af4
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-04-29 11:04:45 -0400

    Update prep_classification_dataset.py

--- src/patched_flannel/classification/prep_classification_dataset.py
+++ src/patched_flannel/classification/prep_classification_dataset.py
@@ -1,9 +1,8 @@
 import splitfolders
 import header
-from src.data_preprocess.segmentation import header as seg_header
 
 splitfolders.ratio(
-    seg_header.dir_save,
+    "./data_preprocess/output/",
     output=header.data_dir,
     seed=1337,
     ratio=(0.7, 0.1, 0.2),
