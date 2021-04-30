7e520b17b9c743fcbe36d92e40b399594a91fc66
srasi1 <srasi1@gmail.com>
2021-04-30 01:17:37 -0400

    Final version without bar chart create

--- src/FLANNEL/utils/generate_bar.py
+++ src/FLANNEL/utils/generate_bar.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Update results home value
 results_home = '/Users/sreddyasi/CS598/pw/results/'
-base_learners = ["densenet161", "inception_v3", "resnet152", "resnext101_32x8d", "vgg19_bn"]
+base_learners = ["densenet161", "inception_v3", "resnet152", "resnext101_32x8d", "vgg19_bn", "ensemble"]
 types = ["Covid-19", "Pneumonia Virus", "Pneumonia Bacteria", "Normal"]
 
 # create cv index for each learner
@@ -13,6 +13,7 @@ cv_index = []
 for i in range(num_folds):
     fold = i + 1
     cv_index.append('cv' + str(fold))
+
 
 # function to compute f1 score and macro-f1 score for each fold and each learner (for each file)
 def compute_f1score(df):
@@ -25,6 +26,7 @@ def compute_f1score(df):
     f1_df['Macro_F1_Score'] = f1_df.mean(axis=1)
     return f1_df
 
+
 # Wrapper function to read P & R, call F1 compute to create DF with for a base learner
 def create_f1df(learner):
     learner_f1 = pd.DataFrame(columns=types, index=cv_index)
@@ -32,7 +34,10 @@ def create_f1df(learner):
     for ind in cv_index:
         result_home = results_home + learner + '/' + ind
         # print(result_home)
-        raw_df = pd.read_csv(result_home + '/measure_detail_' + learner + '_test_' + ind + '.csv')
+        if learner == 'ensemble':
+            raw_df = pd.read_csv(result_home + '/measure_detail.csv')
+        else:
+            raw_df = pd.read_csv(result_home + '/measure_detail_' + learner + '_test_' + ind + '.csv')
         f1_df = compute_f1score(raw_df)
         # print(f1_df['Macro_F1_Score']['f1'])
         for x_type in types:
@@ -40,6 +45,7 @@ def create_f1df(learner):
         macro_f1_list.append(f1_df['Macro_F1_Score']['f1'])
     learner_f1['Macro_F1_Score'] = macro_f1_list
     return learner_f1
+
 
 f1_scores = []
 sd_scores = []
