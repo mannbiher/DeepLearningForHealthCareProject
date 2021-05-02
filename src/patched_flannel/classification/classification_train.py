640a1b2f23b1f01ecf943de368cff68444c52b71
samsri01 <46404142+samsri01@users.noreply.github.com>
2021-05-01 23:51:45 -0400

    Update classification_train.py

--- src/patched_flannel/classification/classification_train.py
+++ src/patched_flannel/classification/classification_train.py
@@ -4,6 +4,7 @@ import argparse
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()  
+    parser.add_argument("cv", help = "Cross Validation") 
     args = parser.parse_args()
     cv = args.cv
     train.main(cv)
