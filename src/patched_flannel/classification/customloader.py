bf64e4dbe9ab757cf5ef61108e5993893b2f90ac
Maneesh Singh <mann.biher@yahoo.co.in>
2021-05-08 11:01:12 +0000

    Fixing warning for patchbypatch

--- src/patched_flannel/classification/customloader.py
+++ src/patched_flannel/classification/customloader.py
@@ -85,8 +85,8 @@ class COVID_Dataset(data.Dataset):
 
         # print('size', X_patch.shape)
 
-        X_patch_img = self.data_transforms(augmentation(
-            Image.fromarray(X_patch), rand_p=rand_p, mode=self.mode))
+        X_patch_img = self.data_transforms(np.array(augmentation(
+            Image.fromarray(X_patch), rand_p=rand_p, mode=self.mode)))
         X_patch_img_ = np.squeeze(np.asarray(X_patch_img))
 
         X_patch_1 = np.expand_dims(X_patch_img_, axis=0)
