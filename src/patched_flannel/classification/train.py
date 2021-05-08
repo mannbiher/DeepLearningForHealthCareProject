9714761807f7f4cba8d9fd210142b44f3eeac927
Maneesh Singh <mann.biher@yahoo.co.in>
2021-05-08 03:32:25 +0000

    cleanup model save

--- src/patched_flannel/classification/train.py
+++ src/patched_flannel/classification/train.py
@@ -282,6 +282,7 @@ def train_model(model, dataloaders, criterion, optimizer,
                 if epoch_avg > best_avg or epoch%opts.checkpoint_saved_n == 0 or epoch == num_epochs-1:
                     print('Current avg score: %f, Best avg score %f, model saved.' % (
                         epoch_avg, best_avg))
+                    best_avg = max(best_avg, epoch_avg)
                     best_model_wts = copy.deepcopy(model.state_dict())
                     save_checkpoint({
                         'epoch': epoch+1,
@@ -289,7 +290,7 @@ def train_model(model, dataloaders, criterion, optimizer,
                         'model_state_dict': best_model_wts,
                         'optimizer_state_dict': optimizer.state_dict(),
                     }, epoch, epoch_avg > best_avg, opts.checkpoint_dir)
-                    best_avg = epoch_avg
+                    
                 else:
                     print('Model not saved.')
 
