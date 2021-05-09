1f55cc7b9ac3b7f5c326eb772d9f7f0788ac89c0
Maneesh Singh <mann.biher@yahoo.co.in>
2021-05-08 23:50:10 -0400

    Download script for patched

--- cloud_utils/patched_status.py
+++ cloud_utils/patched_status.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 from tabulate import tabulate
 
@@ -24,7 +26,8 @@ def get_results():
             if (filename.startswith(suffix)
                 and 'test' in filename
                     and filename.endswith('.csv')):
-                model, _, fold = filename[len(suffix):-len('.csv')].rsplit('_',2)
+                model, _, fold = filename[len(
+                    suffix):-len('.csv')].rsplit('_', 2)
                 completed_models.add((model, fold))
     return completed_models
 
@@ -37,13 +40,13 @@ def get_checkpoints():
         for content in response['Contents']:
             folder, filename = content['Key'].rsplit('/', 1)
             if filename.startswith(suffix):
-                model, _, fold = filename[len(suffix):-len('.png')].rsplit('_',2)
+                model, _, fold = filename[len(
+                    suffix):-len('.png')].rsplit('_', 2)
                 trained_models.add((model, fold))
             if filename == 'model_best.pth.tar':
-                model, fold = (folder.rsplit('/',1)[-1]
-                    .replace('20200407_patched_','')
-                    .rsplit('_',1))
-                
+                model, fold = (folder.rsplit('/', 1)[-1]
+                               .replace('20200407_patched_', '')
+                               .rsplit('_', 1))
 
                 in_training_models.add((model, fold))
     in_training_models = in_training_models - trained_models
@@ -63,19 +66,43 @@ def get_progress():
     trained_models, in_training_models = get_checkpoints()
     in_progress_models = trained_models - completed_models
     for model, cv in in_progress_models:
-        status_table.append([model,cv,'Inference In-Progress'])
+        status_table.append([model, cv, 'Inference In-Progress'])
     for model, cv in in_training_models:
-        status_table.append([model,cv,'Training In-Progress'])
+        status_table.append([model, cv, 'Training In-Progress'])
     for model, cv in completed_models:
         status_table.append([model, cv, 'Done'])
 
     print('Completed', len(completed_models))
-    print('In-Progess', len(in_training_models| in_progress_models))
+    print('In-Progess', len(in_training_models | in_progress_models))
     return status_table
 
-    
+
+def write_script(commands, path):
+    with open(path, 'w') as f:
+        f.write('#!/usr/bin/env bash\n')
+        for command in commands:
+            f.write(command+'\n')
+
+
+def create_download_script(path, folder_pattern="{}_20200407_patched_{}"):
+    s3_cmd = 'aws s3 cp s3://{}/{} {}'
+    suffix = 'result_detail_'
+    commands = []
+    for response in get_response_iterator(S3_RESULT):
+        for content in response['Contents']:
+            folder, filename = content['Key'].rsplit('/', 1)
+            if (filename.startswith(suffix)
+                    and filename.endswith('.csv')):
+                model, _, fold = filename[len(
+                    suffix):-len('.csv')].rsplit('_', 2)
+                folder = folder_pattern.format(model, fold)
+                copy_path = os.path.join(path, folder, filename)
+                commands.append(s3_cmd.format(
+                    S3_BUCKET, content['Key'], copy_path))
+    write_script(commands, 'patched_download.sh')
+
 
 if __name__ == '__main__':
-    table = get_progress()
-    print_table(table)
-
+    #table = get_progress()
+    # print_table(table)
+    create_download_script('patched_results/results/')
