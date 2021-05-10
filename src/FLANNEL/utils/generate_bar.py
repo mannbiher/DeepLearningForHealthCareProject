1f1dbcc0803c54f2d77914a84e976205b5947229
srasi1 <srasi1@gmail.com>
2021-05-10 10:07:39 -0400

    Updated the plot with Micro-F1 scores vs Covid-19 F1 scores

--- src/FLANNEL/utils/generate_bar.py
+++ src/FLANNEL/utils/generate_bar.py
@@ -19,12 +19,15 @@ for i in range(num_folds):
 # function to compute f1 score and macro-f1 score for each fold and each learner (for each file)
 def compute_f1score(df):
     df.set_index('Type', inplace=True)
+    micro_pr = df.iloc[0].mean()
+    micro_re = df.iloc[1].mean()
     f1_df = pd.DataFrame(columns=types, index=['f1'])
     for x_type in types:
         prec = df[x_type]['Precision']
         recl = df[x_type]['Recall']
         f1_df[x_type]['f1'] = (2 * prec * recl) / (prec + recl)
     f1_df['Macro_F1_Score'] = f1_df.mean(axis=1)
+    f1_df['Micro_F1_Score'] = (2 * micro_pr * micro_re) / (micro_pr + micro_re)
     return f1_df
 
 
@@ -32,6 +35,7 @@ def compute_f1score(df):
 def create_f1df(learner):
     learner_f1 = pd.DataFrame(columns=types, index=cv_index)
     macro_f1_list = []
+    micro_f1_list = []
     for ind in cv_index:
         result_home = results_home + learner + '/' + ind
         # print(result_home)
@@ -44,56 +48,100 @@ def create_f1df(learner):
         for x_type in types:
             learner_f1[x_type][ind] = f1_df[x_type]['f1']
         macro_f1_list.append(f1_df['Macro_F1_Score']['f1'])
+        micro_f1_list.append(f1_df['Micro_F1_Score']['f1'])
     learner_f1['Macro_F1_Score'] = macro_f1_list
+    learner_f1['Micro_F1_Score'] = micro_f1_list
     return learner_f1
-
-
-f1_scores = []
-sd_scores = []
-
-all_f1_scores = pd.DataFrame(columns=types, index=base_learners)
-all_sd_scores = pd.DataFrame(columns=types, index=base_learners)
-
-for base_learner in base_learners:
-    learner_df = create_f1df(base_learner)
-    learner_sd = learner_df.std()
-    learner_f1 = learner_df.mean()
-    # print(learner_f1)
-    # f1_scores.append(learner_f1['Covid-19'])
-    # sd_scores.append(learner_sd[0])
-    for x_type in types:
-        all_f1_scores[x_type][base_learner] = learner_f1[x_type]
-        all_sd_scores[x_type][base_learner] = learner_sd[x_type]
-    f1_scores.append(learner_f1['Macro_F1_Score'])
-    sd_scores.append(learner_sd['Macro_F1_Score'])
-
-all_f1_scores['Macro_F1_Score'] = f1_scores
-all_sd_scores['Macro_F1_Score'] = sd_scores
-
-scores = all_f1_scores['Covid-19'].tolist()
-errors = all_sd_scores['Covid-19'].tolist()
 
 
 def addlabels(x,y):
     for i in range(len(x)):
-        plt.text(i, y[i], str(round(y[i], 4)), ha='right', va='bottom', fontsize='small')
+        plt.text(x[i], y[i], str(round(y[i], 4)), ha='right', va='bottom', fontsize='small')
 
 
-def create_bar(scores, errors):
-    x_pos = [i for i,_ in enumerate(base_learners)]
+def create_bar(scores, errors, micro_scores, micro_errors):
+    #x_pos = [i for i,_ in enumerate(base_learners)]
+    x_pos = [1, 4, 7, 10, 13, 16]
+    x_micro_pos = [2, 5, 8, 11, 14, 17]
     colors = {'densenet161': 'papayawhip', 'inception_v3': 'blanchedalmond', 'vgg19_bn': 'bisque',
               'resnext101_32x8d': 'moccasin', 'resnet152': 'navajowhite', 'ensemble': 'limegreen'}
+    colors_micro = {'densenet161': 'burlywood', 'inception_v3': 'tan', 'vgg19_bn': 'orange',
+                    'resnext101_32x8d': 'orange', 'resnet152': 'goldenrod', 'ensemble': 'forestgreen'}
     fig = plt.figure()
     fig.add_axes([0.1, 0.1, 0.6, 0.75])
     plt.bar(x_pos, scores, color=colors.values(), yerr=errors, width=0.6, linewidth=0.1, figure=fig)
+    plt.bar(x_micro_pos, micro_scores, color=colors_micro.values(), yerr=micro_errors, width=1.0, linewidth=0.1,
+            figure=fig)
     addlabels(x_pos, scores)
-    plt.title("Illustration of COVID-19 F1 score vs rest - Error bars from 5-fold CV")
+    addlabels(x_micro_pos, micro_scores)
+    plt.title("COVID-19 F1 score vs MICRO F1 Score", fontsize=10)
+    plt.ylabel("F1 Scores")
+    #plt.xlabel(base_learner)
     labels = list(colors.keys())
     handles = [plt.Rectangle((0, 0), 1, 1, color=colors[label]) for label in labels]
     plt.legend(handles, labels, loc='upper right', bbox_to_anchor=(1.5, 1))
-    #plt.xticks(x_pos, base_learners)
+    plt.xticks(x_pos, base_learners, rotation=23)
     plt.show()
 
-print(all_f1_scores)
-print(all_sd_scores)
-create_bar(scores, errors)
+
+#f1_scores = []
+#sd_scores = []
+
+#all_f1_scores = pd.DataFrame(columns=types, index=base_learners)
+#all_sd_scores = pd.DataFrame(columns=types, index=base_learners)
+
+#for base_learner in base_learners:
+#    learner_df = create_f1df(base_learner)
+#    learner_sd = learner_df.std()
+#    learner_f1 = learner_df.mean()
+#    # print(learner_f1)
+#    # f1_scores.append(learner_f1['Covid-19'])
+#    # sd_scores.append(learner_sd[0])
+#    for x_type in types:
+#        all_f1_scores[x_type][base_learner] = learner_f1[x_type]
+#        all_sd_scores[x_type][base_learner] = learner_sd[x_type]
+#    f1_scores.append(learner_f1['Macro_F1_Score'])
+#    sd_scores.append(learner_sd['Macro_F1_Score'])
+
+#all_f1_scores['Macro_F1_Score'] = f1_scores
+#all_sd_scores['Macro_F1_Score'] = sd_scores
+
+#scores = all_f1_scores['Covid-19'].tolist()
+#errors = all_sd_scores['Covid-19'].tolist()
+
+
+if __name__ == '__main__':
+    f1_scores = []
+    sd_scores = []
+
+    micro_f1_scores = []
+    micro_sd_scores = []
+
+    all_f1_scores = pd.DataFrame(columns=types, index=base_learners)
+    all_sd_scores = pd.DataFrame(columns=types, index=base_learners)
+
+    for base_learner in base_learners:
+        learner_df = create_f1df(base_learner)
+        learner_sd = learner_df.std()
+        learner_f1 = learner_df.mean()
+        # print(learner_f1)
+        # f1_scores.append(learner_f1['Covid-19'])
+        # sd_scores.append(learner_sd[0])
+        for x_type in types:
+            all_f1_scores[x_type][base_learner] = learner_f1[x_type]
+            all_sd_scores[x_type][base_learner] = learner_sd[x_type]
+        f1_scores.append(learner_f1['Macro_F1_Score'])
+        sd_scores.append(learner_sd['Macro_F1_Score'])
+        micro_f1_scores.append(learner_f1['Micro_F1_Score'])
+    # F1 and SD scores
+    all_f1_scores['Macro_F1_Score'] = f1_scores
+    all_sd_scores['Macro_F1_Score'] = sd_scores
+    all_f1_scores['Micro_F1_Score'] = micro_f1_scores
+    all_sd_scores['Micro_F1_Score'] = micro_sd_scores
+    scores = all_f1_scores['Covid-19'].tolist()
+    errors = all_sd_scores['Covid-19'].tolist()
+    micro_scores = all_f1_scores['Micro_F1_Score'].tolist()
+    micro_errors = all_sd_scores['Micro_F1_Score'].tolist()
+    print(all_f1_scores)
+    print(all_sd_scores)
+    create_bar(scores, errors, micro_scores, micro_errors)
